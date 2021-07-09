1525ff9ef86fd34ffc77f131272670197607172c
Dennis Kim <kimutaidennisbrian@gmail.com>
2021-07-09 16:02:39 +0300

    final letsencrypt configuration

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ if (config.util.getEnv("NODE_ENV" !== "test")) {
     app.use(morgan("combined")); // 'combined' outputs the Apache style LOGs
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 443;
 
 const get = require("./Routes/get");
 const remove = require("./Routes/delete");
