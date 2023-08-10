f280a652f743d3b25e449fe924e8272ae4fd81ed
Dennis Kimutai <kimutaidennisbrian@gmail.com>
2023-08-10 18:18:47 +0300

    bug fixes and minor improvements in code injector

--- Public/assets/scripts/codeInjector.js
+++ Public/assets/scripts/codeInjector.js
@@ -7,35 +7,48 @@ const addscript = document.querySelector(".script");
  * @return {Node} HTML
  *  */
 function parseString(str) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(str, "text/html");
-    return doc.head.children;
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(str, "text/html");
+  return doc.head.children;
 }
 
-if (addscript) {
-    addscript.onclick = function() {
-        const headerscript = document.querySelector(".headerscript").value;
-        const footerscript = document.querySelector(".footerscript").value;
+const headerscript = document.querySelector(".headerscript");
+const footerscript = document.querySelector(".footerscript");
 
-        const scripts = [];
-        const headerscriptsarr = [...parseString(headerscript)].map((child) => child.outerHTML);
-        const footerscriptsarr = [...parseString(footerscript)].map((child) => child.outerHTML);
-        // build scripts
-        headerscriptsarr.forEach((s) => {
-            const placeHeader = { script: s, placement: "header" };
-            scripts.push(placeHeader);
-        });
-        footerscriptsarr.forEach((s) => {
-            const placeFooter = { script: s, placement: "footer" };
-            scripts.push(placeFooter);
-        });
-        sendRequest("POST", "/injectcode", scripts)
-            .then((uploadedscripts) => {
-                load.end();
-            })
-            .catch((err) => {
-                load.error();
-                console.log(`Augh,there was an error!,${err.statusText}`);
-            })
-    };
+function checkValue(e) {
+  headerscript.value.trim().length != 0 || footerscript.value.trim().length != 0
+    ? addscript.removeAttribute("disabled")
+    : addscript.setAttribute("disabled", "");
+}
+
+headerscript.addEventListener("input", checkValue, false);
+footerscript.addEventListener("input", checkValue, false);
+
+if (addscript) {
+  addscript.onclick = function () {
+    const scripts = [];
+    const headerscriptsarr = [...parseString(headerscript.value)].map(
+      (child) => child.outerHTML
+    );
+    const footerscriptsarr = [...parseString(footerscript.value)].map(
+      (child) => child.outerHTML
+    );
+    // build scripts
+    headerscriptsarr.forEach((s) => {
+      const placeHeader = { script: s, placement: "header" };
+      scripts.push(placeHeader);
+    });
+    footerscriptsarr.forEach((s) => {
+      const placeFooter = { script: s, placement: "footer" };
+      scripts.push(placeFooter);
+    });
+    sendRequest("POST", "/injectcode", scripts)
+      .then((uploadedscripts) => {
+        load.end();
+      })
+      .catch((err) => {
+        load.error();
+        console.log(`Augh,there was an error!,${err.statusText}`);
+      });
+  };
 }
