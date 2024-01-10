6a9c8f084106c4abc32cdd7e5c343e23ba44e5c4
Dennis Kim <kimutaidennisbrian@gmail.com>
2024-01-10 15:32:17 +0300

    fix error while parsing text to html

--- Public/assets/scripts/injectedScripts.js
+++ Public/assets/scripts/injectedScripts.js
@@ -1,17 +1,33 @@
+//parse text to html
+function parseStringToHTML(string) {
+  const parser = new DOMParser();
+  return parser.parseFromString(string, "text/html");
+}
+
 //Fetch Scripts
 sendRequest(`GET`, `/getinjectedscripts`)
-    .then((data) => {
-        // loop over the data received from the server and inject in the header and footer section.
-        const scripts = JSON.parse(data)
-        for (const i in scripts) {
-            if (scripts[i].placement === "header") {
-                document.querySelector("head").insertAdjacentElement("beforeend", parseStringToHTML(scripts[i].script).head.firstElementChild);
-            } else {
-                document.querySelector("body").insertAdjacentElement("beforeend", parseStringToHTML(scripts[i].script).head.firstElementChild);
-            }
-        }
-    })
-    .catch((err) => {
-        load.error();
-        console.log(`Augh,there was an error!,${err.statusText}`);
-    });
+  .then((data) => {
+    // loop over the data received from the server and inject in the header and footer section.
+    const scripts = JSON.parse(data);
+    for (const i in scripts) {
+      if (scripts[i].placement === "header") {
+        document
+          .querySelector("head")
+          .insertAdjacentElement(
+            "beforeend",
+            parseStringToHTML(scripts[i].script).head.firstElementChild
+          );
+      } else {
+        document
+          .querySelector("body")
+          .insertAdjacentElement(
+            "beforeend",
+            parseStringToHTML(scripts[i].script).head.firstElementChild
+          );
+      }
+    }
+  })
+  .catch((err) => {
+    load.error();
+    console.log(`Augh,there was an error!,${err.statusText}`);
+  });
