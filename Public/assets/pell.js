fc796bbad2f0339449f42c03f20f72838055f57f
Dennis Kimutai <kimutaidennisbrian@gmail.com>
2024-02-09 13:27:05 +0300

    Add functionality to get caret position

--- Public/assets/pell.js
+++ Public/assets/pell.js
@@ -492,9 +492,9 @@
           const filename = getFilenameAndExtension(images[i].name)[0];
 
           let figure = document.createElement("figure");
-          figure.contentEditable = false;
+          //figure.contentEditable = false;
           let figCaption = document.createElement("figcaption");
-          figCaption.contentEditable = false;
+          // figCaption.contentEditable = false;
 
           figure.style.backgroundImage = `url(${url})`;
           figure.setAttribute("class", `i-${i}`);
@@ -661,7 +661,69 @@
       // settings.onChange(content.innerHTML);
     };
 
+
+
+    // node_walk: walk the element tree, stop when func(node) returns false
+    function node_walk(node, func) {
+
+      var result = func(node);
+      for (node = node.firstChild; result !== false && node; node = node.nextSibling)
+
+        result = node_walk(node, func);
+      return result;
+    };
+
+    // getCaretPosition: return [start, end] as offsets to elem.textContent that
+    //   correspond to the selected portion of text
+    //   (if start == end, caret is at given position and no text is selected)
+    function getCaretPosition(elem) {
+      var sel = window.getSelection();
+      var cum_length = [0, 0];
+
+      //first letter type: elem is contenteditable which is equal to sel.anchorNode.Will return cum_length as [0,0]
+      if (sel.anchorNode == elem)
+        cum_length = [sel.anchorOffset, sel.extentOffset];
+
+      else {
+        //second letter type onwards will return sel.anchorNode as the type,i.e text written
+        var nodes_to_find = [sel.anchorNode, sel.extentNode];
+
+        //check if the contenteditable contains the textnode or return undefined
+        if (!elem.contains(sel.anchorNode) || !elem.contains(sel.extentNode))
+          return undefined;
+        else {
+          var found = [0, 0];
+          var i;
+          node_walk(elem, function (node) {
+            for (i = 0; i < 2; i++) {
+              // On first and second iteration,node which is contenteditable will not be equal to the textnode(or element selected)
+              // node_walk will recall itself until when the node will be equal to textnode(or element selected)
+              if (node == nodes_to_find[i]) {
+                found[i] = true;
+                if (found[i == 0 ? 1 : 0])
+                  return false; // all done
+              }
+
+            }
+
+            if (node.textContent && !node.firstChild) {
+              for (i = 0; i < 2; i++) {
+                if (!found[i])
+                  cum_length[i] += node.textContent.length;
+              }
+            }
+          });
+          cum_length[0] += sel.anchorOffset;
+          cum_length[1] += sel.extentOffset;
+        }
+      }
+      if (cum_length[0] <= cum_length[1])
+        return cum_length;
+      return [cum_length[1], cum_length[0]];
+    }
+
     content.onkeydown = function (event) {
+
       if (
         event.key === "Enter" &&
         queryCommandValue(formatBlock) === "blockquote"
@@ -671,6 +733,7 @@
           0
         );
       }
+
     };
 
     appendChild(settings.element, content);
@@ -728,3 +791,4 @@
 
   Object.defineProperty(exports, "__esModule", { value: true });
 });
+
