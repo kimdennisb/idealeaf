8ac2ed2d2a120e02d070cfce60c2f3f184b6676a
Dennis Kimutai <kimutaidennisbrian@gmail.com>
2024-04-30 13:49:08 +0300

    only perform search when input field has a value

--- Public/assets/scripts/sidebarHelper.js
+++ Public/assets/scripts/sidebarHelper.js
@@ -121,16 +121,21 @@ if (searchBox) {
     // set new or modify existing parameter value
     queryParams.set("s", this.value);
     history.replaceState(null, null, `?${queryParams.toString()}`);
+
     // remove all query params when input is empty
     if (this.value === "") {
       history.replaceState(null, "", location.href.split("?")[0]);
     }
 
-    // eslint-disable-next-line no-use-before-define
-    performSearch();
+    //only perform search when input field has a value
+    if (this.value !== "") {
+      performSearch();
+    }
+
   };
 }
 
+//perform searching when url already has queryparams
 if (window.location.search) {
   // eslint-disable-next-line no-use-before-define
   performSearch();
@@ -482,7 +487,7 @@ if (toggleAccess) {
   };
 }
 
-function setRemoveSignInSignUpModal() {}
+function setRemoveSignInSignUpModal() { }
 
 const clearMessageSession = document.querySelector(".clearMessage");
 if (clearMessageSession) {
@@ -519,40 +524,14 @@ parentFigure.forEach((figure) => {
   const prev = figure.querySelector("span[class='prev']");
   next
     ? next.addEventListener("click", function (e) {
-        plusSlides(1);
-      })
+      plusSlides(1);
+    })
     : null;
   prev
     ? prev.addEventListener("click", function (e) {
-        plusSlides(-1);
-      })
+      plusSlides(-1);
+    })
     : null;
 });
 
-/*Dark and Light mode */
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-const toggleSwitch = document.querySelector(".switch__input");
-const currentTheme = localStorage.getItem("theme");
-
-if (currentTheme) {
-  document.documentElement.setAttribute("data-mode", currentTheme);
-
-  if (currentTheme === "dark") {
-    toggleSwitch.checked = true;
-  }
-} else if (prefersDarkScheme.matches) {
-  toggleSwitch.checked = true;
-}
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-mode", "dark");
-    localStorage.setItem("theme", "dark");
-  } else {
-    document.documentElement.setAttribute("data-mode", "light");
-    localStorage.setItem("theme", "light");
-  }
-}
-
-toggleSwitch.addEventListener("change", switchTheme, false);
